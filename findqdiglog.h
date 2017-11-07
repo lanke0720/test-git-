cb8d964eaeb357d66aa872af93b0a11e6c0d6016
lanke0720 <1600977813@qq.com>
2017-11-07 03:18:27 -0800

    top_find

---
+++ findqdiglog.h
@@ -0,0 +1,53 @@
+// #ifndef QDIALOG_H
+// #define QDIALOG_H
+// #pragma once
+//
+// #include <QtGui/QMainWindow>
+// #include <qdialog.h>
+// //#include "ui_qdialog.h"
+//
+// //class QDialog : public QMainWindow
+// //{
+// //	Q_OBJECT
+// //
+// //public:
+// //	QDialog(QWidget *parent = 0, Qt::WFlags flags = 0);
+// //	~QDialog();
+// //
+// //private:
+// //	Ui::QDialogClass ui;
+// //};
+//
+// class QCheckBox;
+// class QLabel;
+// class QLineEdit;
+// class QPushButton;
+//
+// class FindDialog : public QDialog
+// {
+// 	Q_OBJECT
+// 	public:
+// 		FindDialog(QWidget *parent = 0);
+//
+// 			
+// 				signals:
+// 						void findNext(const QString &str, Qt::CaseSensitivity cs);
+// 								void findPrevious (const Qstring &str, Qt::CaseSensitivity cs);
+// 									
+// 										private slots:
+// 												void findClicked();
+// 														void enableFindButton();
+// 															private:
+// 																	QLabel *label;
+// 																			QLineEdit *lineEdit;
+// 																					QCheckBox *caseCheckBox;
+// 																							QCheckBox *backwardcheckBox;
+// 																									QPushButton *findButton;
+// 																											QPushButton *closeButton;
+// 																												
+//
+//
+// 																												};
+//
+// 																												//#endif // QDIALOG_H
+//
